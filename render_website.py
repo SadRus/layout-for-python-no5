bdffbdef1182a87901998aeb3efb133cb43dc84d
SadRus <rystixx@yandex.ru>
2023-03-13 00:29:33 +0300

    Refactoring code

--- render_website.py
+++ render_website.py
@@ -14,41 +14,33 @@ def on_reload():
     )
     template = env.get_template('template.html')
 
-    load_dotenv()
+    STATIC_PREFIX = os.getenv('STATIC_PREFIX', default='../static/')
+    MEDIA_PREFIX = os.getenv('MEDIA_PREFIX', default='../media/')
     book_cards_per_page = int(os.getenv('BOOK_CARDS_PER_PAGE', default=10))
     file_path = os.getenv('FILEPATH', default='./')
-    file_name = os.getenv('FILENAME', default='books_content.json')
-    full_path = os.path.join(file_path, file_name)
+    full_path = os.path.join(file_path, 'books_description.json')
 
     with open(full_path, 'r') as file:
-        books_content = json.load(file)
+        books_description = json.load(file)
 
-    for book_content in books_content:
-        if book_content['img_src'] == 'nopic.gif':
-            book_content['img_src'] = os.path.join(
-                '../static',
-                book_content['img_src']
-            )
-        else:
-            book_content['img_src'] = os.path.join(
-                '../media/images',
-                book_content['img_src']
-            )
     os.makedirs('./pages', exist_ok=True)
-    book_cards_by_page = list(chunked(books_content, book_cards_per_page))
+    book_cards_by_page = list(chunked(books_description, book_cards_per_page))
     pages_total = len(book_cards_by_page)
 
-    for page_num, books_content in enumerate(book_cards_by_page, start=1):
+    for page_num, books_description in enumerate(book_cards_by_page, start=1):
         rendered_page = template.render(
-            book_cards=books_content,
+            book_cards=books_description,
             current_page_num=page_num,
             pages_total=pages_total,
+            STATIC_PREFIX=STATIC_PREFIX,
+            MEDIA_PREFIX=MEDIA_PREFIX,
             )
         with open(f'pages/index{page_num}.html', 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
 if __name__ == '__main__':
+    load_dotenv()
     server = Server()
     server.watch('template.html', on_reload)
     server.serve(root='.')
